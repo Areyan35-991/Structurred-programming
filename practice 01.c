a074e24aa621a0ecdcb03c1d7a48abea33d88d04
Areyan35-991 <106174602+Areyan35-991@users.noreply.github.com>
2022-05-26 00:05:31 +0600

    Add files via upload

---
+++ practice 01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () {
+
+    int number1, number2, sum;
+
+    printf("Enter the value of number1:");
+    scanf("%d", &number1);
+    printf("Enter the value of number2:");
+    scanf("%d", &number2);
+
+    sum = number1 + number2;
+
+    printf("Addition of number1 and number2 :%d\n", sum);
+
+
+	return 0;
+
+}
