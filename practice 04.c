5154067947923b9ac96f9961c331ec0937e8447c
Areyan35-991 <106174602+Areyan35-991@users.noreply.github.com>
2022-05-27 22:31:52 +0600

    Add files via upload

---
+++ practice 04.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+
+	int number1,number2;
+
+	printf("Enter the value of number1:");
+	scanf("%d",&number1);
+	printf("Enter the value of number2:");
+	scanf("%d",&number2);
+
+	if(number1>number2){
+		printf("number1 is bigger");
+	}
+	else{
+		printf("number2 is bigger");
+
+	}
+
+
+	return 0;
+
+
+}
