7aa876e846cf63a7031b226c893a21d0e0badbbc
Areyan35-991 <106174602+Areyan35-991@users.noreply.github.com>
2022-06-19 02:08:23 +0600

    Add files via upload

---
+++ practice 16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+   int i,n,sum=0,odd;
+
+   printf("Input number of terms : ");
+   scanf("%d",&n);
+   printf("\nThe odd numbers are :");
+   for(i=1;i<=n;i++)
+   { odd=2*i-1;
+     printf("%d ",odd);
+     sum+=odd;
+   }
+   printf("\nThe Sum of odd Natural Number upto %d terms : %d \n",n,sum);
+}
